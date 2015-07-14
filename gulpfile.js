f8937ad5116419b5accf720abf358a5410a216a5
mhartington <mikehartington@gmail.com>
2015-07-14 11:20:58 -0400

    add build process, fix inputs with prefilled data Closes #6

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
-
+var tag_version = require('gulp-tag-version');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
 
 gulp.task('default', ['sass', 'js']);
-
+gulp.task('build', ['sass', 'js']);
 
 gulp.task('sass', function(done) {
   gulp.src('./ion-md-input.scss')
@@ -26,17 +28,43 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-
-gulp.task('js', function () {
+gulp.task('js', function() {
   return gulp.src('./ion-md-input.js')
-  .pipe(ngAnnotate())
-  .pipe(gulp.dest('./js'))
-  .pipe(gulp.dest('./demo/www/js'))
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest('./demo/www/js'))
 
   .pipe(uglify())
-  .pipe(rename({
-    extname: '.min.js'
-  }))
-  .pipe(gulp.dest('./js'))
-  .pipe(gulp.dest('./demo/www/js'));
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest('./demo/www/js'));
+});
+
+function inc(importance) {
+  // get all the files to bump version in
+  return gulp.src(['./package.json', './bower.json'])
+    // bump the version number in those files
+    .pipe(bump({
+      type: importance
+    }))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumps package version'))
+  // read only one file to get the version number
+  .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+}
+
+gulp.task('patch', ['build'], function() {
+  return inc('patch');
+});
+gulp.task('minor', ['build'], function() {
+  return inc('minor');
+});
+gulp.task('major', ['build'], function() {
+  return inc('major');
 });
